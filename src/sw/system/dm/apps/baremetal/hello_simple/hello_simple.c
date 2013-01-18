3cd496b03484951a74f4f7cfd8f565c7ba3b24d1
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2013-01-18 09:49:00 +0100

    Add simple hello world for baremetal and establish standard build files needed.

---
+++ src/sw/system/dm/apps/baremetal/hello_simple/hello_simple.c
@@ -0,0 +1,5 @@
+#include <printf.h>
+
+void main() {
+	printf("Hello World!\n");
+}
---
+++ tbench/rtl/dm/compute_tile/sw/sysconfig.c
@@ -0,0 +1,11 @@
+const unsigned int optimsoc_system_clock = 0; // Not needed
+const unsigned int optimsoc_ticks = 0; // Not needed
+const unsigned char optimsoc_has_hostlink = 0; // Needed at later stages
+const unsigned short optimsoc_hostlink = 0; // Not needed
+const unsigned int optimsoc_mainmem_size = 0; // Not needed here
+
+const unsigned int optimsoc_compute_tile_num = 1;
+const unsigned int optimsoc_compute_tiles[1] = { 0 };
+
+// Maximum packet size in flits
+extern const unsigned int optimsoc_noc_maxpacketsize = 0; // Not needed here
