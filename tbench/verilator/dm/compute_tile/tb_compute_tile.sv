820fbe5db4648f2341119dd709de5a835dcb8fde
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2013-06-03 14:35:32 +0200

    Testbench: Import latest verilated
    Update the verilator testbench to the latest work:
    
     * Adapted SystemC layer
    
     * Minor fixes (Philipp Wagner):
    
       * Coding style changes
    
       * Makefile adopted to specify correct link order
    
       * Verilog testbench adapted to upper-case parameters etc.

--- tbench/verilator/dm/compute_tile/tb_compute_tile.sv
+++ tbench/verilator/dm/compute_tile/tb_compute_tile.sv
@@ -1,3 +1,33 @@
+/**
+ * This file is part of OpTiMSoC.
+ *
+ * OpTiMSoC is free hardware: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * As the LGPL in general applies to software, the meaning of
+ * "linking" is defined as using the OpTiMSoC in your projects at
+ * the external interfaces.
+ *
+ * OpTiMSoC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with OpTiMSoC. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * =================================================================
+ *
+ * A verilated testbench for a simple systeme with only one compute tile.
+ *
+ * (c) 2012-2013 by the author(s)
+ *
+ * Author(s):
+ *    Stefan Wallentowitz, stefan.wallentowitz@tum.de
+ */
+
 `include "timescale.v"
 
 module tb_compute_tile(/*AUTOARG*/
@@ -5,45 +35,41 @@ module tb_compute_tile(/*AUTOARG*/
    clk, rst_sys, rst_cpu, cpu_stall
    );
 
+   // NoC parameters
+   parameter NOC_FLIT_DATA_WIDTH = 32;
+   parameter NOC_FLIT_TYPE_WIDTH = 2;
+   localparam NOC_FLIT_WIDTH = NOC_FLIT_DATA_WIDTH + NOC_FLIT_TYPE_WIDTH;
+   parameter VCHANNELS = 3;
+
    input clk;
    input rst_sys;
    input rst_cpu;
    input cpu_stall;
-   
-   parameter noc_flit_data_width = 32;
-   parameter noc_flit_type_width = 2;
-   localparam noc_flit_width = noc_flit_data_width+noc_flit_type_width;
-   parameter vchannels = 3;
 
-   reg [noc_flit_width-1:0] noc_in_flit;
-   reg [vchannels-1:0] noc_in_valid;
-   wire [vchannels-1:0] noc_in_ready;
-   wire [noc_flit_width-1:0] noc_out_flit;
-   wire [vchannels-1:0] noc_out_valid;
-   reg [vchannels-1:0] noc_out_ready;
-
-   parameter id       = 0;
-   parameter cores    = 1;
-   /* memory size in bytes */
-   parameter mem_size = 1*1024*1024; // 4 MByte
-   parameter mem_file = "ct.vmem";
+   reg [NOC_FLIT_WIDTH-1:0] noc_in_flit;
+   reg [VCHANNELS-1:0] noc_in_valid;
+   wire [VCHANNELS-1:0] noc_in_ready;
+   wire [NOC_FLIT_WIDTH-1:0] noc_out_flit;
+   wire [VCHANNELS-1:0] noc_out_valid;
+   reg [VCHANNELS-1:0] noc_out_ready;
 
    compute_tile_dm
-     #(.id(id),.cores(cores),
-       .mem_size(mem_size),.mem_file(mem_file))
-     u_compute_tile(/*AUTOINST*/
-                    // Outputs
-                    .noc_in_ready       (noc_in_ready[vchannels-1:0]),
-                    .noc_out_flit       (noc_out_flit[noc_flit_width-1:0]),
-                    .noc_out_valid      (noc_out_valid[vchannels-1:0]),
-                    // Inputs
-                    .clk                (clk),
-                    .rst_cpu            (rst_cpu),
-                    .rst_sys            (rst_sys),
-                    .noc_in_flit        (noc_in_flit[noc_flit_width-1:0]),
-                    .noc_in_valid       (noc_in_valid[vchannels-1:0]),
-                    .noc_out_ready      (noc_out_ready[vchannels-1:0]),
-                    .cpu_stall          (cpu_stall));
+      #(.ID(0),
+        .CORES(1),
+        .MEM_SIZE(1*1024*1024), // 1 MB
+        .MEM_FILE("ct.vmem"))
+      u_compute_tile(// Outputs
+                     .noc_in_ready      (noc_in_ready[VCHANNELS-1:0]),
+                     .noc_out_flit      (noc_out_flit[NOC_FLIT_WIDTH-1:0]),
+                     .noc_out_valid     (noc_out_valid[VCHANNELS-1:0]),
+                     // Inputs
+                     .clk               (clk),
+                     .rst_cpu           (rst_cpu),
+                     .rst_sys           (rst_sys),
+                     .noc_in_flit       (noc_in_flit[NOC_FLIT_WIDTH-1:0]),
+                     .noc_in_valid      (noc_in_valid[VCHANNELS-1:0]),
+                     .noc_out_ready     (noc_out_ready[VCHANNELS-1:0]),
+                     .cpu_stall         (cpu_stall));
 
 endmodule // tb_system_2x2_ccmc
 
@@ -51,3 +77,4 @@ endmodule // tb_system_2x2_ccmc
 // verilog-library-directories:("." "../../../../src/rtl/*/verilog")
 // verilog-auto-inst-param-value: t
 // End:
+
