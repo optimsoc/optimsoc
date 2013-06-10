7dd6f5d57b470be18fe73e137889588b4b648611
Philipp Wagner <philipp.wagner@tum.de>
2013-06-10 11:39:11 +0200

    SystemC: Add license headers

--- src/sysc/src/debug/DebugConnector.h
+++ src/sysc/src/debug/DebugConnector.h
@@ -1,3 +1,28 @@
+/*
+ * This file is part of OpTiMSoC.
+ *
+ * OpTiMSoC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * OpTiMSoC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with OpTiMSoC. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * ============================================================================
+ *
+ * (c) 2013 by the author(s)
+ *
+ * Author(s):
+ *    Stefan Wallentowitz, stefan.wallentowitz@tum.de
+ */
+
 #ifndef DEBUGCONNECTOR_H_
 #define DEBUGCONNECTOR_H_
 
