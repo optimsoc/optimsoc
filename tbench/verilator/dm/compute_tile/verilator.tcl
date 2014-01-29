ad240117c0b303a1014d94d1593679d328537e47
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-01-29 14:03:26 +0100

    Verilator: Add TCL scripting

---
+++ tbench/verilator/dm/compute_tile/verilator.tcl
@@ -0,0 +1,14 @@
+set OPTIMSOC_TCL $env(OPTIMSOC_TCL)
+
+source $OPTIMSOC_TCL/optimsoc_verilator.tcl
+
+optimsoc_inc_dir .
+optimsoc_inc_dir $OPTIMSOC_RTL
+
+optimsoc_add_file tb_compute_tile.sv
+optimsoc_add_module compute_tile_dm
+optimsoc_add_module trace_monitor.r3_checker
+
+optimsoc_build_define OR1200_BOOT_ADR=32'h100
+
+optimsoc_build
