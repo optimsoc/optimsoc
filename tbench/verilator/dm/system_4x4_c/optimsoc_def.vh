0f76d6d5da232be3233f10d7a074078963dc01bf
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-01-30 14:35:03 +0100

    Tbench-Systems: 4x4, 16 compute tiles, DM

---
+++ tbench/verilator/dm/system_4x4_c/optimsoc_def.vh
@@ -0,0 +1,48 @@
+/**
+ * This file is part of OpTiMSoC.
+ * 
+ * OpTiMSoC is free hardware: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * As the LGPL in general applies to software, the meaning of
+ * "linking" is defined as using the OpTiMSoC in your projects at
+ * the external interfaces.
+ * 
+ * OpTiMSoC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with OpTiMSoC. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * =================================================================
+ * 
+ * System-wide definitions for an OpTiMSoC system instance.
+ * 
+ * (c) 2013 by the author(s)
+ * 
+ * Author(s):
+ *    Stefan Wallentowitz, stefan.wallentowitz@tum.de
+ */
+
+// Number of virtual channels
+`define VCHANNELS 3
+
+// Assign virtual channels to services
+`define VCHANNEL_LSU_REQ   'x
+`define VCHANNEL_LSU_RESP  'x
+`define VCHANNEL_DMA_REQ  0
+`define VCHANNEL_DMA_RESP 1
+`define VCHANNEL_MPSIMPLE 2
+
+`define OPTIMSOC_XDIM 4
+`define OPTIMSOC_YDIM 4
+`define OPTIMSOC_MEMORYID  'x
+
+`define OPTIMSOC_SRAM_IMPLEMENTATION "PLAIN"
+
+`define OPTIMSOC_DEBUG_ENABLE_ITM 1
+`define OPTIMSOC_DEBUG_ENABLE_STM 1
---
+++ tbench/verilator/dm/system_4x4_c/tb_system_4x4_c.sv
@@ -0,0 +1,61 @@
+`include "dbg_config.vh"
+
+module tb_system_4x4_c(/*AUTOARG*/
+   // Inputs
+   clk, rst_sys, rst_cpu
+   );
+
+   input clk;
+   input rst_sys;
+   input rst_cpu;
+
+   localparam NUMCORES = 16;
+   
+   wire [`DEBUG_ITM_PORTWIDTH*NUMCORES-1:0] trace_itm;
+   wire [`DEBUG_STM_PORTWIDTH*NUMCORES-1:0] trace_stm;
+
+   wire [`DEBUG_STM_PORTWIDTH-1:0] trace_stm_array [0:NUMCORES-1]; 
+   wire                            trace_stm_enable [0:NUMCORES-1] /*verilator public_flat_rd*/;
+   wire [31:0]                     trace_stm_insn [0:NUMCORES-1] /*verilator public_flat_rd*/;
+   wire                            trace_stm_wben [0:NUMCORES-1];
+   wire [4:0]                      trace_stm_wbreg [0:NUMCORES-1];
+   wire [31:0]                     trace_stm_wbdata [0:NUMCORES-1];
+   wire [31:0]                     trace_stm_r3 [0:NUMCORES-1] /*verilator public_flat_rd*/;
+   
+   genvar                          i;
+   
+   generate
+      for (i = 0; i < NUMCORES; i++) begin
+         assign trace_stm_array[i] = trace_stm[(i+1)*`DEBUG_STM_PORTWIDTH-1:`DEBUG_STM_PORTWIDTH*i];
+         assign trace_stm_enable[i] = trace_stm_array[i][`DEBUG_STM_ENABLE_MSB:`DEBUG_STM_ENABLE_LSB];
+         assign trace_stm_insn[i] = trace_stm_array[i][`DEBUG_STM_INSN_MSB:`DEBUG_STM_INSN_LSB];
+         assign trace_stm_wben[i] = trace_stm_array[i][`DEBUG_STM_WB_MSB:`DEBUG_STM_WB_LSB];
+         assign trace_stm_wbreg[i] = trace_stm_array[i][`DEBUG_STM_WBREG_MSB:`DEBUG_STM_WBREG_LSB];
+         assign trace_stm_wbdata[i] = trace_stm_array[i][`DEBUG_STM_WBDATA_MSB:`DEBUG_STM_WBDATA_LSB];
+
+         r3_checker
+           u_r3_checker(.clk(clk),
+                        .valid(trace_stm_enable[i]),
+                        .we (trace_stm_wben[i]),
+                        .addr (trace_stm_wbreg[i]),
+                        .data (trace_stm_wbdata[i]),
+                        .r3 (trace_stm_r3[i]));
+      end
+   endgenerate
+   
+   system_4x4_c_dm
+     #(.MEM_SIZE(1*1024*1024))
+     u_system(
+              .clk                      (clk),
+              .rst_sys                  (rst_sys),
+              .rst_cpu                  (rst_cpu),
+              .trace_itm                (trace_itm),
+              .trace_stm                (trace_stm));
+  
+   
+endmodule
+
+// Local Variables:
+// verilog-library-directories:("." "../../../../src/rtl/*/verilog")
+// verilog-auto-inst-param-value: t
+// End:
