697be1634f0f2f6ce31dd63c4d190a8cdec385ea
Philipp Wagner <philipp.wagner@tum.de>
2014-02-21 19:20:06 +0100

    GUI: Use pkg-config to find liboptimsochost

--- src/sw/host/optimsocgui/cmake/FindLiboptimsochost.cmake
+++ src/sw/host/optimsocgui/cmake/FindLiboptimsochost.cmake
@@ -1,6 +1,6 @@
 #-*-cmake-*-
 #
-# Copyright (c) 2012-2013 by the author(s)
+# Copyright (c) 2012-2014 by the author(s)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -27,35 +27,45 @@
 #
 # Once loaded this will define
 #  LIBOPTIMSOCHOST_FOUND        - system has liboptimsochost
-#  LIBOPTIMSOCHOST_INCLUDE_DIR  - include directory for liboptimsochost
+#  LIBOPTIMSOCHOST_INCLUDE_DIRS - include directory for liboptimsochost
 #  LIBOPTIMSOCHOST_LIBRARIES    - libraries you need to link to
+#  LIBOPTIMSOCHOST_DEFINITIONS  - required compiler switches for using this lib
 #
 
-set(LIBOPTIMSOCHOST_FOUND   "NO")
+find_package(PkgConfig)
+pkg_check_modules(PC_LIBOPTIMSOCHOST QUIET liboptimsochost)
+set(LIBOPTIMSOCHOST_DEFINITIONS ${PC_LIBOPTIMSOCHOST_CFLAGS_OTHER})
 
+# HINTS gets searched before the system locations, PATHS after these
 find_path(LIBOPTIMSOCHOST_INCLUDE_DIR liboptimsochost.h
-  "$ENV{LIBOPTIMSOCHOST_LOCATION}/src/optimsochost"
-  "$ENV{OPTIMSOC}/src/sw/host/liboptimsochost/src/optimsochost"
-  "${PROJECT_SOURCE_DIR}/../liboptimsochost/src/optimsochost"
-  /usr/include/optimsochost
+  HINTS
+    "${PC_LIBOPTIMSOCHOST_INCLUDEDIR}"
+    "${PC_LIBOPTIMSOCHOST_INCLUDE_DIRS}"
+  PATHS
+    "$ENV{LIBOPTIMSOCHOST_LOCATION}/src"
+    "$ENV{OPTIMSOC}/src/sw/host/liboptimsochost/src"
+    "${PROJECT_SOURCE_DIR}/../liboptimsochost/src"
 )
 
 find_library(LIBOPTIMSOCHOST_LIBRARY
   NAMES optimsochost
+  HINTS
+    "${PC_LIBOPTIMSOCHOST_LIBDIR}"
+    "${PC_LIBOPTIMSOCHOST_LIBRARY_DIRS}"
   PATHS
-  "$ENV{LIBOPTIMSOCHOST_LOCATION}/src/.libs"
-  "$ENV{OPTIMSOC}/src/sw/host/liboptimsochost/lib"
-  "${PROJECT_SOURCE_DIR}/../liboptimsochost/lib"
+    "$ENV{LIBOPTIMSOCHOST_LOCATION}/src/.libs"
+    "$ENV{OPTIMSOC}/src/sw/host/liboptimsochost/lib"
+    "${PROJECT_SOURCE_DIR}/../liboptimsochost/lib"
   DOC "liboptimsochost library"
 )
+
+
+set(LIBOPTIMSOCHOST_LIBRARIES ${LIBOPTIMSOCHOST_LIBRARY})
+set(LIBOPTIMSOCHOST_INCLUDE_DIRS ${LIBOPTIMSOCHOST_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LIBOPTIMSOCHOST DEFAULT_MSG
                                   LIBOPTIMSOCHOST_LIBRARY
                                   LIBOPTIMSOCHOST_INCLUDE_DIR)
-
-if(LIBOPTIMSOCHOST_FOUND)
-  set(LIBOPTIMSOCHOST_LIBRARIES ${LIBOPTIMSOCHOST_LIBRARY})
-endif(LIBOPTIMSOCHOST_FOUND)
 
 mark_as_advanced(LIBOPTIMSOCHOST_LIBRARY LIBOPTIMSOCHOST_INCLUDE_DIR)
