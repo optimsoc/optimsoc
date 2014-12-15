ea9573ed4ac726518f0bda94d95fd596b943fcc3
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-12-15 14:19:29 +0100

    SystemSW: Rework implementation of send and recv

--- src/sw/system/baremetal-libs/src/libmp/mp.c
+++ src/sw/system/baremetal-libs/src/libmp/mp.c
@@ -10,6 +10,7 @@ typedef struct endpoint_handle* optimsoc_mp_endpoint_handle;
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 /*
  * All message passing functions are entered here from the software. The general
@@ -53,6 +54,8 @@ int optimsoc_mp_endpoint_create(struct endpoint_handle **eph,
                                 optimsoc_endpoint_type buffer_type,
                                 uint32_t buffer_size, int overwrite_max_size) {
 
+    // Endpoint creation only requires access to the local database, no
+    // messages between the tiles
     *eph = endpoint_create(node, port, buffer_type, buffer_size, overwrite_max_size);
 
     return 0;
@@ -60,6 +63,8 @@ int optimsoc_mp_endpoint_create(struct endpoint_handle **eph,
 
 int optimsoc_mp_endpoint_get(struct endpoint_handle **eph, uint32_t tile,
                              uint32_t node, uint32_t port) {
+    // This gets the endpoint from the local database or creates a request to
+    // the tile if it is not found
     *eph = endpoint_get(tile, node, port);
     return 0;
 }
@@ -75,18 +80,28 @@ int optimsoc_mp_channel_connect(struct endpoint_handle *from,
     return 0;
 }
 
-int optimsoc_mp_channel_recv(struct endpoint_handle *ep,
+int optimsoc_mp_channel_recv(struct endpoint_handle *eph,
                            uint8_t* buffer, uint32_t buffer_size,
                            uint32_t *received_size) {
+    int ret = 0;
+    uint32_t read_ptr;
 
-    while(endpoint_empty(ep->ep)) { }
+    while(endpoint_empty(eph->ep)) { }
 
-    // TODO: fill buffer
-    ep->ep->buffer->read_ptr = _endpoint_addptrwrap(ep->ep, ep->ep->buffer->read_ptr, 1);
-    trace_ep_bufferstate(ep->ep, endpoint_channel_get_fillstate(ep->ep));
+    endpoint_pop(eph->ep, &read_ptr);
+    trace_ep_bufferstate(eph->ep, endpoint_channel_get_fillstate(eph->ep));
 
-    control_channel_sendcredit(ep, 1);
-    return 0;
+    *received_size = eph->ep->buffer->data_size[read_ptr];
+    if (*received_size <= buffer_size) {
+        memcpy(buffer, (void*)eph->ep->buffer->data[read_ptr], *received_size);
+    } else {
+        memcpy(buffer, (void*)eph->ep->buffer->data[read_ptr], buffer_size);
+        ret = OPTIMSOC_MP_ERROR_BUFFEROVERFLOW;
+    }
+
+    control_channel_sendcredit(eph, 1);
+
+    return ret;
 }
 
 int optimsoc_mp_channel_pause(struct endpoint_handle *ep) {
@@ -103,21 +118,35 @@ int optimsoc_mp_channel_send(struct endpoint_handle *from,
                            struct endpoint_handle *to,
                            uint8_t* buffer,
                            uint32_t size) {
-
+    //printf("%d\n", from->ep->remotecredit);
+    trace_chan_send_begin(from, size);
     if (from->ep->remotecredit == 0) {
         if (endpoint_full(from->ep)) {
+            assert(0==1);
             while (from->ep->remotecredit == 0) {}
 
             printf("TODO: Drain sender\n");
         }
 
-        while (from->ep->remotecredit == 0) {}
+        while (from->ep->remotecredit == 0) { }
     }
 
-    control_channel_send(to, buffer, size);
+    uint32_t oldcredit;
 
-    // TODO: CAS!
-    from->ep->remotecredit--;
+    do {
+        oldcredit = from->ep->remotecredit;
+    } while (or1k_sync_cas((void*)&(from->ep->remotecredit), oldcredit, oldcredit-1) != oldcredit);
+
+    trace_chan_send_xmit(from);
+
+    if (_optimsoc_mp_data == OPTIMSOC_MP_DATA_FIFO) {
+        control_channel_send(to, buffer, size);
+    } else {
+//        dma_transfer()
+//        control_channel_send(to, buffer, size);
+    }
+
+    trace_chan_send_end(from);
 
     return 0;
 }
