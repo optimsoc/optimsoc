11e5078f365381ab9db0809320379a1ffe0b52c5
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-03-23 21:33:27 +0100

    gzll-libs: Initial drafting of the userspace

---
+++ src/sw/system/gzll-libs/src/libgzll/gzll.c
@@ -0,0 +1,13 @@
+
+
+#include <stdlib.h>
+
+#include <gzll-syscall.h>
+
+#include "syscall.h"
+
+void gzll_get_kernel_version(char **version_string) {
+	version_string = memalign(GZLL_KERNEL_VERSION_STRING_MAXLENGTH+1);
+
+	syscall(GZLL_SYSCALL_KERNEL_INFO, (uint32_t) version_string, 0, 0, 0, 0, 0);
+}
---
+++ src/sw/system/gzll-libs/src/libgzll/include/gzll.h
@@ -0,0 +1,6 @@
+#ifndef __GZLL_H__
+#define __GZLL_H__
+
+void gzll_get_kernel_version(char **version_string);
+
+#endif
