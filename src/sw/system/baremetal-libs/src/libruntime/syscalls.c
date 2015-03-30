dffab2d7d9bc5141c2311c662b730e2f09cb6e84
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-03-30 13:32:46 +0200

    SystemSW/runtime: syscall namespace

--- src/sw/system/baremetal-libs/src/libruntime/syscalls.c
+++ src/sw/system/baremetal-libs/src/libruntime/syscalls.c
@@ -2,17 +2,16 @@
 #include "include/optimsoc-runtime.h"
 #include "syscalls.h"
 
-struct optimsoc_runtime_syscall *_optimsoc_runtime_syscall_data;
+struct optimsoc_syscall *_optimsoc_syscall_data;
 
-void _optimsoc_runtime_syscalls_init() {
+void _optimsoc_syscalls_init() {
 	// By default no exception handler is set
 	or1k_exception_handler_add(0xc, (or1k_exception_handler_fptr) -1);
 
-	void* c = calloc(or1k_numcores(), sizeof(struct optimsoc_runtime_syscall));
-	_optimsoc_runtime_syscall_data = (struct optimsoc_runtime_syscall*) c;
+	void* c = calloc(or1k_numcores(), sizeof(struct optimsoc_syscall));
+	_optimsoc_syscall_data = (struct optimsoc_syscall*) c;
 }
 
-void optimsoc_runtime_syscall_handler_set(
-		optimsoc_runtime_syscall_handler_fptr handler) {
+void optimsoc_syscall_handler_set(optimsoc_syscall_handler_fptr handler) {
 	or1k_exception_handler_add(0xc, &_optimsoc_runtime_sycall_entry);
 }
