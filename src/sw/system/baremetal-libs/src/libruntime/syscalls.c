a2df85e7e5f3cd7bfd10fa42e1f50e84e2a9cd9e
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-04-03 17:50:42 +0200

    SystemSW/runtime: Fix syscall handler
    Implement the handler registration properly, correctly calculate the
    handler address in assembly.

--- src/sw/system/baremetal-libs/src/libruntime/syscalls.c
+++ src/sw/system/baremetal-libs/src/libruntime/syscalls.c
@@ -1,17 +1,23 @@
+#include <assert.h>
+
 #include <optimsoc-baremetal.h>
 #include "include/optimsoc-runtime.h"
 #include "syscalls.h"
 
+/*! Pointer to the array of structs which contains one syscall per core. */
 struct optimsoc_syscall *_optimsoc_syscall_data;
 
+optimsoc_syscall_handler_fptr _optimsoc_syscall_handler;
+
 void _optimsoc_syscalls_init() {
-	// By default no exception handler is set
-	or1k_exception_handler_add(0xc, (or1k_exception_handler_fptr) -1);
+	// Set the exception handler for syscall exceptions
+    or1k_exception_handler_add(0xc, &_optimsoc_sycall_entry);
 
 	void* c = calloc(or1k_numcores(), sizeof(struct optimsoc_syscall));
+	assert(c);
 	_optimsoc_syscall_data = (struct optimsoc_syscall*) c;
 }
 
 void optimsoc_syscall_handler_set(optimsoc_syscall_handler_fptr handler) {
-	or1k_exception_handler_add(0xc, &_optimsoc_runtime_sycall_entry);
+    _optimsoc_syscall_handler = handler;
 }
