ed3f9fc8f591e755a507d8d2ca8166aad0d297bc
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-04-15 09:43:30 +0200

    gzll-libs: More prototyping

--- src/sw/system/gzll-libs/src/libgzll/syscall.h
+++ src/sw/system/gzll-libs/src/libgzll/syscall.h
@@ -40,10 +40,8 @@ static inline uint32_t syscall(uint32_t id, uint32_t p0, uint32_t p1,
 	register uint32_t r7 __asm__("r7") = p4;
 	register uint32_t r8 __asm__("r8") = p5;
 
-	__asm__ __volatile__ ("l.sys 0"
-			      : "=r"(r11)
-			      : "r"(r11), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8)
-			      : "memory");
+	__asm__ __volatile__ ("l.sys 0" : "=r"(r11) : "r"(r11), "r"(r3), "r"(r4),
+			"r"(r5), "r"(r6), "r"(r7), "r"(r8) : "memory");
 
 	return r11;
 }
