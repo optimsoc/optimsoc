faec5a78d9592e7fc5c71c04b2d7a9d6f15459af
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-04-19 18:51:13 +0200

    gzll: Also allocate stack

--- src/sw/system/gzll-kernel/src/task.c
+++ src/sw/system/gzll-kernel/src/task.c
@@ -8,6 +8,8 @@
 // TODO: load from global memory
 
 void gzll_task_start(char* identifier, struct gzll_task *task) {
+    int rv;
+
     printf("Boot task %s\n", identifier);
 
     unsigned int size = task->obj_end - task->obj_start;
@@ -19,7 +21,7 @@ void gzll_task_start(char* identifier, struct gzll_task *task) {
 
     unsigned int num_pages = (size + 8191) >> 13;
     for (unsigned int p = 0; p < num_pages; p++) {
-        int alloced = gzll_page_alloc();
+        unsigned int alloced = gzll_page_alloc();
         assert(alloced);
 
         void *from = task->obj_start + p * 8192;
@@ -32,8 +34,16 @@ void gzll_task_start(char* identifier, struct gzll_task *task) {
 
         printf(" - load %d bytes from %p to %p\n", len, from, to);
         memcpy(to, from, len);
-        optimsoc_vmm_map(pdir, (uint32_t) p + 0x2000, (uint32_t) to);
+
+        rv = optimsoc_vmm_map(pdir, (uint32_t) (p << 13) + 0x2000,
+                              (uint32_t) to);
+        assert(rv == 1);
     }
+
+    // Create a stack
+    unsigned int alloced = gzll_page_alloc();
+    assert(alloced);
+    optimsoc_vmm_map(pdir, 0xfffffffc, alloced << 13);
 
     optimsoc_thread_t thread = malloc(sizeof(optimsoc_thread_t));
     optimsoc_thread_create(&thread, (void*) 0x2000, 0);
