34745867311e1d469783162c2e63f0cad81a204a
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-05-20 18:41:51 +0200

    gzll-libs: Proper pointer handling for version

--- src/sw/system/gzll-libs/src/libgzll/gzll.c
+++ src/sw/system/gzll-libs/src/libgzll/gzll.c
@@ -7,8 +7,8 @@
 #include "syscall.h"
 
 void gzll_get_kernel_version(char **version_string) {
-	version_string = malloc(GZLL_KERNEL_VERSION_STRING_MAXLENGTH+1);
+	*version_string = malloc(GZLL_KERNEL_VERSION_STRING_MAXLENGTH+1);
 
-	syscall(GZLL_SYSCALL_KERNEL_INFO, (uint32_t) version_string, 0, 0, 0, 0, 0);
+	syscall(GZLL_SYSCALL_KERNEL_INFO, (uint32_t) *version_string, 0, 0, 0, 0, 0);
 }
 
--- src/sw/system/gzll-libs/src/libgzll/syscall.h
+++ src/sw/system/gzll-libs/src/libgzll/syscall.h
@@ -27,7 +27,7 @@
 #ifndef __SYSCALL_H__
 #define __SYSCALL_H__
 
-#include <inttypes.h>
+#include <stdint.h>
 
 static inline uint32_t syscall(uint32_t id, uint32_t p0, uint32_t p1,
 		uint32_t p2, uint32_t p3, uint32_t p4, uint32_t p5)
