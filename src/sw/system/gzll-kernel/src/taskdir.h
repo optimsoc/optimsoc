e4b082f7f83eb1327ba3248373daaf298726c27e
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-05-21 17:24:48 +0200

    gzll: Change lookup structure

--- src/sw/system/gzll-kernel/src/taskdir.h
+++ src/sw/system/gzll-kernel/src/taskdir.h
@@ -5,44 +5,43 @@
 
 extern uint32_t *taskdir_table[];
 
+struct gzll_app_node {
+    uint32_t rank;
+    uint32_t nodeid;
+};
+
+struct gzll_app_taskdir {
+    unsigned int size;
+    struct gzll_app_node *tasks;
+};
+
 /**
  * Initialize
  */
-void taskdir_initialize();
+void taskdir_initialize(struct gzll_app_taskdir *dir);
 
 /**
  * Delete an task
  * @param taskid the ID of the task
  * @return error code
  */
-int taskdir_task_delete(uint32_t taskid);
+int taskdir_task_delete(struct gzll_app_taskdir *dir, uint32_t taskid);
 
 /**
  * Register a new remote task
  * @param taskid the ID of the task
  * @return error code
  */
-int taskdir_task_register(uint32_t taskid, uint32_t tile);
+int taskdir_task_register(struct gzll_app_taskdir *dir, uint32_t taskid,
+                          uint32_t tile, uint32_t node);
 
 /**
  * Remap a task to a new tile
  *
  */
-int taskdir_task_remap(uint32_t taskid, uint32_t old_tileid, uint32_t new_tileid);
-
-/**
- * Checks if an task ID is not locally assigned
- * @param taskid the ID of the task
- * @return error code
- */
-int taskdir_taskid_available(uint32_t taskid);
-
-/**
- * Reserve the task ID entry
- * @param taskid the ID of the task
- * @return error code
- */
-int taskdir_taskid_reserve(uint32_t taskid);
+int taskdir_task_remap(struct gzll_app_taskdir *dir, uint32_t taskid,
+                       uint32_t old_tileid, uint32_t new_tileid,
+                       uint32_t old_nodeid, uint32_t new_nodeid);
 
 /**
  * Query the location of the task
@@ -50,6 +49,7 @@ int taskdir_taskid_reserve(uint32_t taskid);
  * @param *tileid a pointer to return the tile ID
  * @return error code
  */
-int taskdir_tileid_lookup(uint32_t taskid, uint32_t *tileid);
+int taskdir_tileid_lookup(struct gzll_app_taskdir *dir, uint32_t taskid,
+                          uint32_t *tileid, uint32_t nodeid);
 
 #endif //__TASKDIR_H__
