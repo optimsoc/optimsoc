ae37b8f8bb77278aa7372c30c6b02e28f30d4b6d
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-05-21 20:15:05 +0200

    SystemSW/runtime: Helper function to print context

--- src/sw/system/baremetal-libs/src/libruntime/context.c
+++ src/sw/system/baremetal-libs/src/libruntime/context.c
@@ -19,6 +19,27 @@ void _optimsoc_context_init(void) {
     }
 }
 
+void _optimsoc_context_print(_optimsoc_thread_ctx_t *ctx) {
+    printf("context %p:\n", ctx);
+    printf("  r0:  0x%08x r1:  0x%08x r2:  0x%08x r3:  0x%08x\n", ctx->regs[0],
+           ctx->regs[1], ctx->regs[2], ctx->regs[3]);
+    printf("  r4:  0x%08x r5:  0x%08x r6:  0x%08x r7:  0x%08x\n", ctx->regs[4],
+           ctx->regs[5], ctx->regs[6], ctx->regs[7]);
+    printf("  r8:  0x%08x r9:  0x%08x r10: 0x%08x r11: 0x%08x\n", ctx->regs[8],
+           ctx->regs[9], ctx->regs[10], ctx->regs[11]);
+    printf("  r12: 0x%08x r13: 0x%08x r14: 0x%08x r15: 0x%08x\n", ctx->regs[12],
+           ctx->regs[13], ctx->regs[14], ctx->regs[15]);
+    printf("  r16: 0x%08x r17: 0x%08x r18: 0x%08x r19: 0x%08x\n", ctx->regs[16],
+           ctx->regs[17], ctx->regs[18], ctx->regs[19]);
+    printf("  r20: 0x%08x r21: 0x%08x r22: 0x%08x r23: 0x%08x\n", ctx->regs[20],
+           ctx->regs[21], ctx->regs[22], ctx->regs[23]);
+    printf("  r24: 0x%08x r25: 0x%08x r26: 0x%08x r27: 0x%08x\n", ctx->regs[24],
+           ctx->regs[25], ctx->regs[26], ctx->regs[27]);
+    printf("  r28: 0x%08x r29: 0x%08x r30: 0x%08x r31: 0x%08x\n", ctx->regs[28],
+           ctx->regs[29], ctx->regs[30], ctx->regs[31]);
+    printf("  PC: 0x%08x SR: 0x%08x\n", ctx->pc, ctx->sr);
+}
+
 void _optimsoc_context_copy(_optimsoc_thread_ctx_t *to,
                             _optimsoc_thread_ctx_t *from) {
     size_t size = sizeof(struct _optimsoc_thread_ctx_t);
