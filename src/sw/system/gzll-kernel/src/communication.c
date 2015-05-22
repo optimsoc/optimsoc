fcf30017625580a9bbe8c26a657721d43bad900f
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-05-22 12:16:04 +0200

    gzll: Add first draft for inter-rank messages

--- src/sw/system/gzll-kernel/src/communication.c
+++ src/sw/system/gzll-kernel/src/communication.c
@@ -1,16 +1,25 @@
 
 #include <stdio.h>
+#include <assert.h>
 
 #include <optimsoc-baremetal.h>
 #include <optimsoc-runtime.h>
 #include <optimsoc-mp.h>
 
 #include "gzll.h"
+#include "messages.h"
 
 optimsoc_mp_endpoint_handle _gzll_mp_ep_system;
 optimsoc_mp_endpoint_handle *_gzll_mp_ep_system_remote;
 
+typedef void (*gzll_message_handler_fptr)(struct gzll_message *);
+
+gzll_message_handler_fptr gzll_message_handlers[GZLL_NUM_MESSAGE_TYPES];
+
 void communication_init() {
+    // Initialize function pointers
+    gzll_message_handlers[GZLL_NODE_NEW] = &gzll_message_node_new_handler;
+
     optimsoc_mp_endpoint_create(&_gzll_mp_ep_system, 0, 0,
                                 OPTIMSOC_MP_EP_CONNECTIONLESS, 32, 64);
 
@@ -34,7 +43,57 @@ void communication_init() {
 }
 
 void communication_thread() {
+    uint8_t buffer[256];
+    struct gzll_message *msg = (struct gzll_message*) buffer;
+
     printf("Communication thread started\n");
-//    while (1) { asm("l.nop"); }
+
+    while (1) {
+        uint32_t received;
+        optimsoc_mp_msg_recv(_gzll_mp_ep_system, buffer, 256, &received);
+        assert((received > 1) && (received == msg->len));
+        assert(msg->type < GZLL_NUM_MESSAGE_TYPES);
+        gzll_message_handlers[msg->type](msg);
+    }
+
+}
+
+void message_send_node_new(uint32_t app_id, uint32_t app_nodeid,
+                           uint32_t nodeid) {
+    uint32_t msg_size;
+
+    msg_size = sizeof(struct gzll_message) +
+            sizeof(struct gzll_message_node_new);
+
+    struct gzll_message *msg;
+    struct gzll_message_node_new *msg_node_new;
+
+    msg = calloc(1, msg_size);
+    msg->len = msg_size;
+    msg->source_rank = gzll_rank;
+    msg->type = GZLL_NODE_NEW;
+
+    msg_node_new = (struct gzll_message_node_new*) &msg->data[0];
+    msg_node_new->app_id = app_id;
+    msg_node_new->app_nodeid = app_nodeid;
+
+    for (int r = 0; r < optimsoc_get_numct(); ++r) {
+        if (r == gzll_rank) {
+            continue;
+        }
+        optimsoc_mp_msg_send(_gzll_mp_ep_system, _gzll_mp_ep_system_remote[r],
+                             (uint8_t*) msg, msg_size);
+    }
+    free(msg);
+}
+
+void gzll_message_node_new_handler(struct gzll_message *msg) {
+    struct gzll_message_node_new *msg_node;
+    msg_node = (struct gzll_message_node_new*) msg->data;
+
+    printf("Received new node information\n");
+    printf("  on rank: %d\n", msg->source_rank);
+    printf("  appid: %d, nodeid: %d, ranknode: %d\n", msg_node->app_id,
+           msg_node->app_nodeid, msg_node->rank_nodeid);
 
 }
---
+++ src/sw/system/gzll-kernel/src/messages.h
@@ -0,0 +1,28 @@
+#ifndef SRC_MESSAGES_H_
+#define SRC_MESSAGES_H_
+
+void message_send_node_new(uint32_t appid, uint32_t app_nodeid, uint32_t nodeid);
+
+#define GZLL_NUM_MESSAGE_TYPES 1
+
+enum gzll_message_types {
+    GZLL_NODE_NEW
+};
+
+
+struct gzll_message {
+    uint32_t type;
+    uint32_t source_rank;
+    uint32_t len;
+    uint8_t data[0];
+};
+
+struct gzll_message_node_new {
+    uint32_t app_id;
+    uint32_t app_nodeid;
+    uint32_t rank_nodeid;
+};
+
+void gzll_message_node_new_handler(struct gzll_message *msg);
+
+#endif /* SRC_MESSAGES_H_ */
