3053ee27fe6951402ae4095c7d52bdc33865e8ab
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-05-25 16:53:13 +0200

    gzll-libs: Add name lookup syscall

--- src/sw/system/gzll-libs/src/libgzll/gzll.c
+++ src/sw/system/gzll-libs/src/libgzll/gzll.c
@@ -1,14 +1,21 @@
 
-
 #include <stdlib.h>
+#include <string.h>
 
+#include "include/gzll.h"
 #include <gzll-syscall.h>
 
 #include "syscall.h"
 
 void gzll_get_kernel_version(char **version_string) {
-	*version_string = malloc(GZLL_KERNEL_VERSION_STRING_MAXLENGTH+1);
+    *version_string = malloc(GZLL_KERNEL_VERSION_STRING_MAXLENGTH+1);
 
-	syscall(GZLL_SYSCALL_KERNEL_INFO, (uint32_t) *version_string, 0, 0, 0, 0, 0);
+    syscall(GZLL_SYSCALL_KERNEL_INFO, (uint32_t) *version_string, 0, 0, 0, 0, 0);
 }
 
+int gzll_lookup_nodeid(char *name, gzll_node_id *nodeid) {
+    uint32_t len = strlen(name) + 1;
+
+    return syscall(GZLL_SYSCALL_NODE_LOOKUP, (uint32_t) name, len,
+                   (uint32_t) nodeid, 0, 0, 0);
+}
--- src/sw/system/gzll-libs/src/libgzll/include/gzll.h
+++ src/sw/system/gzll-libs/src/libgzll/include/gzll.h
@@ -1,6 +1,16 @@
 #ifndef __GZLL_H__
 #define __GZLL_H__
 
+#include <stdint.h>
+
 void gzll_get_kernel_version(char **version_string);
+
+typedef uint32_t gzll_node_id;
+int gzll_lookup_nodeid(char *name, gzll_node_id *nodeid);
+
+typedef struct gzll_mp_endpoint* gzll_mp_endpoint_t;
+
+int gzll_mp_endpoint_get(gzll_mp_endpoint_t *ep, gzll_node_id node,
+                         uint32_t port);
 
 #endif
