16f05760cb843d8fbbf43038e9be9451985dfa41
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-05-25 16:50:23 +0200

    apps/gzll: pingpong extend by name lookup

--- src/sw/system/apps/gzll/apps/pingpong/receiver.c
+++ src/sw/system/apps/gzll/apps/pingpong/receiver.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include <gzll.h>
 
 void main() {
-  printf("Receiver started\n");
+	int rv;
+	gzll_node_id sender_node;
+
+	printf("Receiver started\n");
+
+	rv = gzll_lookup_nodeid("sender-0", &sender_node);
+	assert(rv == 0);
+
+	printf("Looked up receiver id: %d\n", sender_node);
 }
--- src/sw/system/apps/gzll/apps/pingpong/sender.c
+++ src/sw/system/apps/gzll/apps/pingpong/sender.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include <gzll.h>
 
 void main() {
-  printf("Sender started\n");
+	int rv;
+	gzll_node_id receiver_node;
+	printf("Sender started\n");
+
+	rv = gzll_lookup_nodeid("receiver-0", &receiver_node);
+	assert(rv == 0);
+
+	printf("Looked up receiver id: %d\n", receiver_node);
 }
