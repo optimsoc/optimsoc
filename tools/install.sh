6f0641093d9e8ba78a1a0603d728b77d1a6eb668
Ana Petrovska <ana.petrovska@tum.de>
2015-10-06 16:22:59 +0200

    tools: Remove old tools from installation

--- tools/install.sh
+++ tools/install.sh
@@ -12,10 +12,9 @@ cp -r $OPTIMSOC/tools/tcl $1/tools
 make -C $OPTIMSOC/tools/utils > /dev/null
 mkdir -p $1/tools/utils
 cp $OPTIMSOC/tools/utils/bin2vmem $1/tools/utils
-cp $OPTIMSOC/tools/utils/optimsoc-app-link $1/tools/utils
-cp $OPTIMSOC/tools/utils/optimsoc-app-copy $1/tools/utils
 cp $OPTIMSOC/tools/utils/optimsoc-pgas-binary $1/tools/utils
 
 echo "++ Done. Be sure to set the following environment variables"
 echo "++  OPTIMSOC_TCL=$1/tools/tcl"
 echo "++  PATH=\$PATH:$1/tools/utils"
+
