6c880a2aa47f5a42771538042ac94e2dff781c8b
Stefan Wallentowitz <stefan@wallentowitz.de>
2015-12-27 13:25:20 +0100

    Installation: Add baremetal

--- tools/install.py
+++ tools/install.py
@@ -4,7 +4,6 @@ from optparse import OptionParser
 import os
 import subprocess
 import shutil
-import errno
 
 def info(msg):
     print("(I) {}".format(msg))
@@ -16,11 +15,54 @@ def fatal(msg):
 def check_base_permission(base):
     return os.access(base, os.F_OK | os.R_OK | os.W_OK | os.X_OK)
 
-def check_make():
+def check_program(program):
     try:
-        subprocess.check_output("make --version", stderr=subprocess.STDOUT, shell=True)
+        subprocess.check_output("{} --version".format(program), stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError:
+        fatal("'{}' not found".format(program))
+    
+def check_make():
+    check_program("make")
+
+def check_or1kelf():
+    check_program("or1k-elf-gcc")
+
+def check_autotools():
+    check_program("autoconf")
+    check_program("automake")
+    check_program("autoreconf")
+
+def run_command(cmd, **kwargs):
+    try:
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, **kwargs)
     except subprocess.CalledProcessError as e:
-        fatal("'make' not found")
+        fatal("make error: {}\n{}".format(e, e.output))
+    
+
+def create_base(options):
+    base = options.dest
+    force = options.force
+    
+    if not os.access(base, os.F_OK):
+        # The base folder does not exist
+        try:
+            # Try to create it
+            os.makedirs(base)
+        except os.error as e:
+            fatal("Cannot create base: {}".format(e))
+    else:
+        # The base folder exists, check permission
+        if not check_base_permission(base):
+            fatal("Cannot write to '{}'."
+                  "You need full permissions.".format(base))
+        listdir = os.listdir(base)
+        if listdir:
+            # The folder is not empty
+            if not force:
+                fatal("The destination '{}' is not empty. Use -f to force install.".format(base))
+            info("Destination not empty. Force clean.")
+            for d in listdir:
+                shutil.rmtree(os.path.join(base, d))
 
 def install_tools(options):
     src = options.src
@@ -52,46 +94,54 @@ def install_tools(options):
     os.mkdir(utilsdestdir)
 
     try:
-        info("   + make")
+        info("  + Build")
         makecmd = "make -C {}".format(utilssrcdir)
-        try:
-            subprocess.check_output(makecmd, stderr=subprocess.STDOUT, shell=True)
-        except subprocess.CalledProcessError as e:
-            fatal("make error: {}\n{}".format(e, e.output))
+        run_command(makecmd)
     except Exception as e:
         fatal(e)
     
-    info("   + copy")
+    info("  + Install")
     utilsfiles = ['bin2vmem', 'optimsoc-pgas-binary']
     for f in utilsfiles:
         srcf = os.path.join(utilssrcdir, f) 
         destf = os.path.join(utilsdestdir, f) 
         shutil.copy(srcf, destf)
 
-def create_base(options):
-    base = options.dest
-    force = options.force
+def install_soc_libraries(options):
+    src = options.src
+    dest = options.dest
     
-    if not os.access(base, os.F_OK):
-        # The base folder does not exist
+    info("Install SoC libraries")
+    check_autotools()
+    check_or1kelf()
+    
+    libs = {}
+    libs["baremetal-libs"] = {}
+    
+    libssrc = os.path.join(src, "src", "sw", "system")
+    libsdest = os.path.join(dest, "sw")
+    
+    for lib in libs:
+        info(" + {}".format(lib))
+        info("  + autogen")
+        cmd = "./autogen.sh"
+        cwd = os.path.join(libssrc, lib)
+        run_command(cmd, cwd=cwd)
+        
+        info("  + Configure")
+        cwdbuild = os.path.join(cwd, "build")
         try:
-            # Try to create it
-            os.makedirs(base)
-        except os.error as e:
-            fatal("Cannot create base: {}".format(e))
-    else:
-        # The base folder exists, check permission
-        if not check_base_permission(base):
-            fatal("Cannot write to '{}'."
-                  "You need full permissions.".format(base))
-        listdir = os.listdir(base)
-        if listdir:
-            # The folder is not empty
-            if not force:
-                fatal("The destination '{}' is not empty. Use -f to force install.".format(base))
-            info("Destination not empty. Force clean.")
-            for d in listdir:
-                shutil.rmtree(os.path.join(base, d))
+            os.mkdir(cwdbuild)
+        except OSError as e:
+            shutil.rmtree(cwdbuild)
+            os.mkdir(cwdbuild)
+        
+        cmd = "../configure --prefix={} --host=or1k-elf".format(libsdest)
+        run_command(cmd, cwd=cwdbuild)
+
+        info("  + Build and Install")
+        cmd = "make install"
+        run_command(cmd, cwd=cwdbuild)
 
 if __name__ == '__main__':
     scriptname = os.path.realpath(__file__)
@@ -114,3 +164,7 @@ if __name__ == '__main__':
     create_base(options)
 
     install_tools(options)
+    
+    install_soc_libraries(options)
+    
+    info("Done")
