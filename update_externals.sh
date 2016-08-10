96df51ffe2593b894d2ff04846e2392053610d7f
Philipp Wagner <philipp.wagner@tum.de>
2016-08-11 00:14:56 +0200

    fix update_externals.sh

--- update_externals.sh
+++ update_externals.sh
@@ -7,22 +7,25 @@
 git stash
 
 # lisnoc
-git subtree pull --prefix external/lisnoc https://github.com/tum-lis/lisnoc.git master --squash
+git subtree pull -m "Update external/lisnoc" --prefix external/lisnoc https://github.com/tum-lis/lisnoc.git master --squash
 
 # glip
-git subtree pull --prefix external/glip https://github.com/tum-lis/glip.git master --squash
+git subtree pull -m "Update external/glip" --prefix external/glip https://github.com/tum-lis/glip.git master --squash
 
 # extra_cores: additional shared cores (by wallento)
-git subtree pull --prefix external/extra_cores https://github.com/wallento/fusesoc_cores.git master --squash
+git subtree pull -m "Update external/extra_cores" --prefix external/extra_cores https://github.com/wallento/fusesoc_cores.git master --squash
 
 # fusesoc: FuseSoC
-git subtree pull --prefix external/fusesoc https://github.com/wallento/fusesoc.git dev --squash
+git subtree pull -m "Update external/fusesoc" --prefix external/fusesoc https://github.com/wallento/fusesoc.git dev --squash
+
+# fusesoc-ipyxact: Fusesoc dependency
+git subtree pull -m "Update external/fusesoc-ipyxact" --prefix external/fusesoc-ipyxact https://github.com/olofk/ipyxact.git master --squash
 
 # opensocdebug/software: OSD software
-git subtree pull --prefix external/opensocdebug/software https://github.com/opensocdebug/software.git master --squash
+git subtree pull -m "Update external/opensocdebug/software" --prefix external/opensocdebug/software https://github.com/opensocdebug/software.git master --squash
 
 # opensocdebug/hardware: OSD hardware
-git subtree pull --prefix external/opensocdebug/hardware https://github.com/opensocdebug/hardware.git fusesoc --squash
+git subtree pull -m "Update external/opensocdebug/hardware" --prefix external/opensocdebug/hardware https://github.com/opensocdebug/hardware.git fusesoc --squash
 
 # reapply our changes to the working directory
 git stash pop
