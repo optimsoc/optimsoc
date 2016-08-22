4263fac34c3941e7c9d1a353a06608f71b420a79
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-08-22 10:35:53 +0200

    Migrate 2x2 system

--- src/soc/hw/system_2x2_cccc_dm/verilog/system_2x2_cccc_dm.sv
+++ src/soc/hw/system_2x2_cccc_dm/verilog/system_2x2_cccc_dm.sv
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013 by the author(s)
+/* Copyright (c) 2012-2016 by the author(s)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,8 +22,6 @@
  *
  * A 2x2 distributed memory system with four compute tiles (CCCC)
  *
- * (c) 2012-2013 by the author(s)
- *
  * Author(s):
  *   Stefan Wallentowitz <stefan.wallentowitz@tum.de>
  */
@@ -32,74 +30,84 @@
 `include "optimsoc_def.vh"
 `include "dbg_config.vh"
 
-module system_2x2_cccc_dm(
-`ifdef OPTIMSOC_DEBUG_ENABLE_MAM
-   wb_mam_adr_o, wb_mam_cyc_o, wb_mam_dat_o, wb_mam_sel_o, wb_mam_stb_o,
-   wb_mam_we_o, wb_mam_cab_o, wb_mam_cti_o, wb_mam_bte_o, wb_mam_ack_i,
-   wb_mam_rty_i, wb_mam_err_i, wb_mam_dat_i,
-`endif
+module system_2x2_cccc_dm
+  #(// NoC parameters
+    parameter NOC_DATA_WIDTH = 32,
+    parameter NOC_TYPE_WIDTH = 2,
+    parameter VCHANNELS = `VCHANNELS,
+
+    // compute tile parameters
+    parameter MEM_FILE = "ct.vmem",
+    parameter MEM_SIZE = 1*1024*1024, // 1 MByte
+
+    parameter CORES = 1,
+
+    parameter USE_DEBUG = 1
+    )
+   (input clk, rst,
+
+    glip_channel c_glip_in,
+    glip_channel c_glip_out
+
 `ifdef OPTIMSOC_CTRAM_WIRES
-   wb_mem_adr_i, wb_mem_cyc_i, wb_mem_dat_i, wb_mem_sel_i,
-   wb_mem_stb_i, wb_mem_we_i, wb_mem_cab_i, wb_mem_cti_i,
-   wb_mem_bte_i, wb_mem_ack_o, wb_mem_rty_o, wb_mem_err_o,
-   wb_mem_dat_o,
+    , output [4*32-1:0] wb_mem_adr_i,
+    output [4*1-1:0]  wb_mem_cyc_i,
+    output [4*32-1:0] wb_mem_dat_i,
+    output [4*4-1:0]  wb_mem_sel_i,
+    output [4*1-1:0]  wb_mem_stb_i,
+    output [4*1-1:0]  wb_mem_we_i,
+    output [4*1-1:0]  wb_mem_cab_i,
+    output [4*3-1:0]  wb_mem_cti_i,
+    output [4*2-1:0]  wb_mem_bte_i,
+    input [4*1-1:0]   wb_mem_ack_o,
+    input [4*1-1:0]   wb_mem_rty_o,
+    input [4*1-1:0]   wb_mem_err_o,
+    input [4*32-1:0]  wb_mem_dat_o
 `endif
-   /*AUTOARG*/
-   // Outputs
-   trace,
-   // Inputs
-   clk, rst_sys, rst_cpu
-   );
+    );
 
-   // NoC parameters
-   parameter NOC_DATA_WIDTH = 32;
-   parameter NOC_TYPE_WIDTH = 2;
    localparam NOC_FLIT_WIDTH = NOC_DATA_WIDTH + NOC_TYPE_WIDTH;
-   parameter VCHANNELS = `VCHANNELS;
 
-   // compute tile parameters
-   parameter MEM_FILE = "ct.vmem";
-   parameter MEM_SIZE = 1*1024*1024; // 1 MByte
-
-   parameter CORES = 1;
+   localparam DEBUG_MODS_PER_CORE = 2;
+   localparam DEBUG_MODS_PER_TILE = CORES * DEBUG_MODS_PER_CORE + 1;
+   localparam DEBUG_MODS = 4 * DEBUG_MODS_PER_TILE;
 
    localparam NUMCTS = 32'h4;
    localparam [NUMCTS*16-1:0] CTLIST = {16'h0, 16'h1, 16'h2, 16'h3};
 
-   input clk, rst_sys, rst_cpu;
+   dii_flit [1:0] debug_ring_in [0:3];
+   dii_flit [1:0] debug_ring_out [0:3];
+   logic [1:0] debug_ring_in_ready [0:3];
+   logic [1:0] debug_ring_out_ready [0:3];
 
-   output [`DEBUG_TRACE_EXEC_WIDTH*4*CORES-1:0] trace;
+   logic       rst_sys, rst_cpu;
 
-`ifdef OPTIMSOC_DEBUG_ENABLE_MAM
-   input [4*32-1:0]   wb_mam_adr_o;
-   input [4*1-1:0]    wb_mam_cyc_o;
-   input [4*32-1:0]   wb_mam_dat_o;
-   input [4*4-1:0]    wb_mam_sel_o;
-   input [4*1-1:0]    wb_mam_stb_o;
-   input [4*1-1:0]    wb_mam_we_o;
-   input [4*1-1:0]    wb_mam_cab_o;
-   input [4*3-1:0]    wb_mam_cti_o;
-   input [4*2-1:0]    wb_mam_bte_o;
-   output [4*1-1:0]   wb_mam_ack_i;
-   output [4*1-1:0]   wb_mam_rty_i;
-   output [4*1-1:0]   wb_mam_err_i;
-   output [4*32-1:0]  wb_mam_dat_i;
-`endif
-`ifdef OPTIMSOC_CTRAM_WIRES
-   output [4*32-1:0] wb_mem_adr_i;
-   output [4*1-1:0]  wb_mem_cyc_i;
-   output [4*32-1:0] wb_mem_dat_i;
-   output [4*4-1:0]  wb_mem_sel_i;
-   output [4*1-1:0]  wb_mem_stb_i;
-   output [4*1-1:0]  wb_mem_we_i;
-   output [4*1-1:0]  wb_mem_cab_i;
-   output [4*3-1:0]  wb_mem_cti_i;
-   output [4*2-1:0]  wb_mem_bte_i;
-   input [4*1-1:0]   wb_mem_ack_o;
-   input [4*1-1:0]   wb_mem_rty_o;
-   input [4*1-1:0]   wb_mem_err_o;
-   input [4*32-1:0]  wb_mem_dat_o;
-`endif
+   debug_interface
+      #(
+         .SYSTEMID    (1),
+         .NUM_MODULES (DEBUG_MODS)
+      )
+      u_debuginterface
+        (
+         .clk            (clk),
+         .rst            (rst),
+         .sys_rst        (rst_sys),
+         .cpu_rst        (rst_cpu),
+         .glip_in        (c_glip_in),
+         .glip_out       (c_glip_out),
+         .ring_out       (debug_ring_in[0]),
+         .ring_out_ready (debug_ring_in_ready[0]),
+         .ring_in        (debug_ring_out[2]),
+         .ring_in_ready  (debug_ring_out_ready[2])
+      );
+
+   // We are routing the debug in a meander
+   assign debug_ring_in[1] = debug_ring_out[0];
+   assign debug_ring_out_ready[0] = debug_ring_in_ready[1];
+   assign debug_ring_in[3] = debug_ring_out[1];
+   assign debug_ring_out_ready[1] = debug_ring_in_ready[3];
+   assign debug_ring_in[2] = debug_ring_out[3];
+   assign debug_ring_out_ready[3] = debug_ring_in_ready[2];
 
    // Flits from NoC->tiles
    wire [NOC_FLIT_WIDTH-1:0] link_in_flit[0:3];
@@ -158,51 +166,42 @@ module system_2x2_cccc_dm(
               .COREBASE(i*CORES),
               .MEM_SIZE(MEM_SIZE),
               .MEM_FILE(MEM_FILE),
+              .USE_DEBUG(USE_DEBUG),
+              .DEBUG_BASEID(2+i*DEBUG_MODS_PER_TILE),
               .NUMCTS(NUMCTS),
               .CTLIST(CTLIST))
-            u_ct(// Outputs
-                 .trace                      (trace[(`DEBUG_TRACE_EXEC_WIDTH*CORES*(i+1))-1:`DEBUG_TRACE_EXEC_WIDTH*CORES*i]),
-`ifdef OPTIMSOC_DEBUG_ENABLE_MAM
-                 .wb_mam_ack_i               (wb_mam_ack_i[i]),
-                 .wb_mam_rty_i               (wb_mam_rty_i[i]),
-                 .wb_mam_err_i               (wb_mam_err_i[i]),
-                 .wb_mam_dat_i               (wb_mam_dat_i[(i+1)*32-1:i*32]),
-                 .wb_mam_adr_o               (wb_mam_adr_o[(i+1)*32-1:i*32]),
-                 .wb_mam_cyc_o               (wb_mam_cyc_o[i]),
-                 .wb_mam_dat_o               (wb_mam_dat_o[(i+1)*32-1:i*32]),
-                 .wb_mam_sel_o               (wb_mam_sel_o[(i+1)*4-1:i*4]),
-                 .wb_mam_stb_o               (wb_mam_stb_o[i]),
-                 .wb_mam_we_o                (wb_mam_we_o[i]),
-                 .wb_mam_cab_o               (wb_mam_cab_o[i]),
-                 .wb_mam_cti_o               (wb_mam_cti_o[(i+1)*3-1:i*3]),
-                 .wb_mam_bte_o               (wb_mam_bte_o[(i+1)*2-1:i*2]),
-`endif
+         u_ct(.clk                        (clk),
+              .rst_cpu                    (rst_cpu),
+              .rst_sys                    (rst_sys),
+              .rst_dbg                    (rst),
+              .debug_ring_in              (debug_ring_in[i]),
+              .debug_ring_in_ready        (debug_ring_in_ready[i]),
+              .debug_ring_out             (debug_ring_out[i]),
+              .debug_ring_out_ready       (debug_ring_out_ready[i]),
+
 `ifdef OPTIMSOC_CTRAM_WIRES
-                 .wb_mem_ack_o               (wb_mem_ack_o[i]),
-                 .wb_mem_rty_o               (wb_mem_rty_o[i]),
-                 .wb_mem_err_o               (wb_mem_err_o[i]),
-                 .wb_mem_dat_o               (wb_mem_dat_o[(i+1)*32-1:i*32]),
-                 .wb_mem_adr_i               (wb_mem_adr_i[(i+1)*32-1:i*32]),
-                 .wb_mem_cyc_i               (wb_mem_cyc_i[i]),
-                 .wb_mem_dat_i               (wb_mem_dat_i[(i+1)*32-1:i*32]),
-                 .wb_mem_sel_i               (wb_mem_sel_i[(i+1)*4-1:i*4]),
-                 .wb_mem_stb_i               (wb_mem_stb_i[i]),
-                 .wb_mem_we_i                (wb_mem_we_i[i]),
-                 .wb_mem_cab_i               (wb_mem_cab_i[i]),
-                 .wb_mem_cti_i               (wb_mem_cti_i[(i+1)*3-1:i*3]),
-                 .wb_mem_bte_i               (wb_mem_bte_i[(i+1)*2-1:i*2]),
-`endif
-                 .noc_in_ready               (link_in_ready[i][VCHANNELS-1:0]),
-                 .noc_out_flit               (link_out_flit[i][NOC_FLIT_WIDTH-1:0]),
-                 .noc_out_valid              (link_out_valid[i][VCHANNELS-1:0]),
-                 // Inputs
-                 .clk                        (clk),
-                 .rst_cpu                    (rst_cpu),
-                 .rst_sys                    (rst_sys),
-                 .noc_in_flit                (link_in_flit[i][NOC_FLIT_WIDTH-1:0]),
-                 .noc_in_valid               (link_in_valid[i][VCHANNELS-1:0]),
-                 .noc_out_ready              (link_out_ready[i][VCHANNELS-1:0]),
-                 .cpu_stall                  (1'b0));
+              .wb_mem_ack_o               (wb_mem_ack_o[i]),
+              .wb_mem_rty_o               (wb_mem_rty_o[i]),
+              .wb_mem_err_o               (wb_mem_err_o[i]),
+              .wb_mem_dat_o               (wb_mem_dat_o[(i+1)*32-1:i*32]),
+              .wb_mem_adr_i               (wb_mem_adr_i[(i+1)*32-1:i*32]),
+              .wb_mem_cyc_i               (wb_mem_cyc_i[i]),
+              .wb_mem_dat_i               (wb_mem_dat_i[(i+1)*32-1:i*32]),
+              .wb_mem_sel_i               (wb_mem_sel_i[(i+1)*4-1:i*4]),
+              .wb_mem_stb_i               (wb_mem_stb_i[i]),
+              .wb_mem_we_i                (wb_mem_we_i[i]),
+              .wb_mem_cab_i               (wb_mem_cab_i[i]),
+              .wb_mem_cti_i               (wb_mem_cti_i[(i+1)*3-1:i*3]),
+              .wb_mem_bte_i               (wb_mem_bte_i[(i+1)*2-1:i*2]),
+`endif //  `ifdef OPTIMSOC_CTRAM_WIRES
+
+              .noc_in_ready               (link_in_ready[i][VCHANNELS-1:0]),
+              .noc_out_flit               (link_out_flit[i][NOC_FLIT_WIDTH-1:0]),
+              .noc_out_valid              (link_out_valid[i][VCHANNELS-1:0]),
+
+              .noc_in_flit                (link_in_flit[i][NOC_FLIT_WIDTH-1:0]),
+              .noc_in_valid               (link_in_valid[i][VCHANNELS-1:0]),
+              .noc_out_ready              (link_out_ready[i][VCHANNELS-1:0]));
       end
    endgenerate
 
