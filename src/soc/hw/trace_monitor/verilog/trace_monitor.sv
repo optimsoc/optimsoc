afc12017c9a7366d3c8e18cbe2514d0283e683b7
Philipp Wagner <philipp.wagner@tum.de>
2016-11-23 22:19:03 +0100

    Reduce stdout spew due to repeated messages
    Fixes #25

--- src/soc/hw/trace_monitor/verilog/trace_monitor.sv
+++ src/soc/hw/trace_monitor/verilog/trace_monitor.sv
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013 by the author(s)
+/* Copyright (c) 2012-2016 by the author(s)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,13 +20,18 @@
  *
  * ============================================================================
  *
- * The trace monitor is used to collect data during RTL simulation.
+ * Trace actions on mor1kx CPU cores during simulations
+ *
+ * - Record and output the printf() calls to STDOUT using the OpTiMSoC-specific
+ *   "nop" printf() method.
+ * - Record OpTiMSoC trace events (also emitted through the "nop" extensions)
+ * - Record exceptions
+ * - Terminate the simulation if all programs have terminated.
  *
  * Author(s):
  *   Philipp Wagner <philipp.wagner@tum.de>
  *   Stefan Wallentowitz <stefan.wallentowitz@tum.de>
  */
-
 module trace_monitor(/*AUTOARG*/
    // Outputs
    termination,
@@ -57,14 +62,14 @@ module trace_monitor(/*AUTOARG*/
    // Signals of all termination requests of all monitors
    input [TERM_CROSS_NUM-1:0]   termination_all;
 
-   reg [31:0]   cur_pos;
+   reg [31:0]   wb_pc_prev;
    integer      count;
    integer      stdout;
    integer      tracefile;
    reg          is_newline;
 
    initial begin
-      cur_pos = 32'h0000_0000;
+      wb_pc_prev = 32'h0000_0000;
       count = 0;
       is_newline = 1;
 
@@ -81,29 +86,29 @@ module trace_monitor(/*AUTOARG*/
    end
 
    reg terminated = 0;
-   
+
    always @(posedge clk) begin
       if (termination & !terminated) begin
-	 if (&{termination_all}) begin
+         if (&{termination_all}) begin
             if (!terminated) begin
                $finish();
             end
-	 end
+         end
          terminated <= 1;
       end
 
       if (enable) begin
+         wb_pc_prev <= wb_pc;
+
          if (ENABLE_TRACE) begin
-            if ((cur_pos + 4 == wb_pc ) || (cur_pos == wb_pc)) begin
-               count++;
-            end else begin
-               if (count > 0) begin
-                  $fwrite(tracefile, "[%0t, %0d] %3d, 0x%08x\n", $time, ID, count, wb_pc);
-                  $fflush(tracefile);
-                  count <= 0;
-               end
+            if ((wb_pc_prev + 4 == wb_pc) || (wb_pc_prev == wb_pc)) begin
+               count <= count + 1;
+            end 
+            else if (count > 0) begin
+               $fwrite(tracefile, "[%0t, %0d] %3d, 0x%08x\n", $time, ID, count, wb_pc);
+               $fflush(tracefile);
+               count <= 0;
             end
-            cur_pos <= wb_pc;
          end
 
          if (wb_insn[31:16] == 16'h1500) begin
@@ -113,7 +118,7 @@ module trace_monitor(/*AUTOARG*/
               end
               16'h0001: begin
                  $display("[%t, %0d] Terminated at address 0x%x (status: %d)", $time, ID, wb_pc, r3);
-                 termination = 1;
+                 termination <= 1;
               end
               16'h0004: begin
                  // simprint
@@ -134,27 +139,28 @@ module trace_monitor(/*AUTOARG*/
             endcase
          end // if (wb_insn[31:16] == 16'h1500)
          else if ((wb_pc[31:12] == 0) && (wb_pc[7:0] == 0)) begin
-            case (wb_pc[11:8])
-              1: $display("[%t, %0d] Software reset", $time, ID);
-              2: $display("[%t, %0d] Bus error exception", $time, ID);
-              3: $display("[%t, %0d] Data page fault exception", $time, ID);
-              4: $display("[%t, %0d] Instruction page fault exception", $time, ID);
-              5: $display("[%t, %0d] Tick timer interrupt exception", $time, ID);
-              6: $display("[%t, %0d] Alignment exception", $time, ID);
-              7: $display("[%t, %0d] Illegal instruction exception", $time, ID);
-              8: $display("[%t, %0d] External interrupt exception", $time, ID);
-              9: $display("[%t, %0d] D-TLB miss exception", $time, ID);
-              10: $display("[%t, %0d] I-TLB miss exception", $time, ID);
-              11: $display("[%t, %0d] Range exception", $time, ID);
-              12: $display("[%t, %0d] System call exception", $time, ID);
-              13: $display("[%t, %0d] Trap exception", $time, ID);
-            endcase
-         end // if ((wb_pc[31:12] == 0) && (wb_pc[7:0] == 0))
+            // record every exception only when it first occurs, not every cycle
+            if (wb_pc[11:8] != wb_pc_prev[11:8]) begin
+               case (wb_pc[11:8])
+                 1: $display("[%t, %0d] Software reset", $time, ID);
+                 2: $display("[%t, %0d] Bus error exception", $time, ID);
+                 3: $display("[%t, %0d] Data page fault exception", $time, ID);
+                 4: $display("[%t, %0d] Instruction page fault exception", $time, ID);
+                 5: $display("[%t, %0d] Tick timer interrupt exception", $time, ID);
+                 6: $display("[%t, %0d] Alignment exception", $time, ID);
+                 7: $display("[%t, %0d] Illegal instruction exception", $time, ID);
+                 8: $display("[%t, %0d] External interrupt exception", $time, ID);
+                 9: $display("[%t, %0d] D-TLB miss exception", $time, ID);
+                 10: $display("[%t, %0d] I-TLB miss exception", $time, ID);
+                 11: $display("[%t, %0d] Range exception", $time, ID);
+                 12: $display("[%t, %0d] System call exception", $time, ID);
+                 13: $display("[%t, %0d] Trap exception", $time, ID);
+               endcase
+            end
+         end
          else if (wb_insn[31:0] == 32'h24000000) begin
             $display("[%t, %0d] Return from exception", $time, ID);
          end
-
-      end // if (enable)
+      end
    end
-
-endmodule // trace_monitor
+endmodule
