32eaa2a28c6fddeae05416f7bdf248161ed1e8ba
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-04-07 16:52:08 +0200

    HW: Add enable signal to wishbone interconnect
    A slave can be disabled even if its ID is in the activated range
    (<SLAVES). This is useful for instantiation with configurable slaves.

--- src/soc/hw/wb_interconnect/verilog/wb_decode.sv
+++ src/soc/hw/wb_interconnect/verilog/wb_decode.sv
@@ -28,8 +28,8 @@
  *  wb_sselect
  *   #(.DATA_WIDTH(32), .ADDR_WIDTH(32),
  *     .SLAVES(2),
- *     .S0_RANGE_WIDTH(1), .S0_RANGE_MATCH(1'b0),
- *     .S1_RANGE_WIDTH(4), .S1_RANGE_MATCH(4'he))
+ *     .S0_ENABLE (1), .S0_RANGE_WIDTH(1), .S0_RANGE_MATCH(1'b0),
+ *     .S1_ENABLE (1), .S1_RANGE_WIDTH(4), .S1_RANGE_MATCH(4'he))
  *  sselect(.clk_i(clk), rst_i(rst),
  *      .s_adr_o({m_adr_o[3],..,m_adr_o[0]},
  *      ...
@@ -66,6 +66,11 @@
  *  S1_RANGE_WIDTH(2), S1_RANGE_MATCH(2'b10)
  *  S2_RANGE_WIDTH(8), S2_RANGE_MATCH(8'he0)
  *
+ *
+ * Finally, the slaves can be individually masked to ease
+ * instantiation using the Sx_ENABLE parameter. By defaults all slaves
+ * are enabled (and selected as long as x < SLAVES).
+ *
  * Author(s):
  *   Stefan Wallentowitz <stefan.wallentowitz@tum.de>
  */
@@ -85,59 +90,69 @@ module wb_decode
     // The number of parameters actually limits the number of slaves as
     // there is no generic way that is handled by all tools to define
     // variable width parameter arrays.
+    parameter S0_ENABLE = 1,
     parameter S0_RANGE_WIDTH = 1,
     parameter S0_RANGE_MATCH = 1'b0,
+    parameter S1_ENABLE = 1,
     parameter S1_RANGE_WIDTH = 1,
     parameter S1_RANGE_MATCH = 1'b0,
+    parameter S2_ENABLE = 1,
     parameter S2_RANGE_WIDTH = 1,
     parameter S2_RANGE_MATCH = 1'b0,
+    parameter S3_ENABLE = 1,
     parameter S3_RANGE_WIDTH = 1,
     parameter S3_RANGE_MATCH = 1'b0,
+    parameter S4_ENABLE = 1,
     parameter S4_RANGE_WIDTH = 1,
     parameter S4_RANGE_MATCH = 1'b0,
+    parameter S5_ENABLE = 1,
     parameter S5_RANGE_WIDTH = 1,
     parameter S5_RANGE_MATCH = 1'b0,
+    parameter S6_ENABLE = 1,
     parameter S6_RANGE_WIDTH = 1,
     parameter S6_RANGE_MATCH = 1'b0,
+    parameter S7_ENABLE = 1,
     parameter S7_RANGE_WIDTH = 1,
     parameter S7_RANGE_MATCH = 1'b0,
+    parameter S8_ENABLE = 1,
     parameter S8_RANGE_WIDTH = 1,
     parameter S8_RANGE_MATCH = 1'b0,
+    parameter S9_ENABLE = 1,
     parameter S9_RANGE_WIDTH = 1,
     parameter S9_RANGE_MATCH = 1'b0
    )
    (
     /* Ports */
-    input 			       clk_i,
-    input 			       rst_i,
+    input                              clk_i,
+    input                              rst_i,
    
-    input [ADDR_WIDTH-1:0] 	       m_adr_i,
-    input [DATA_WIDTH-1:0] 	       m_dat_i,
-    input 			       m_cyc_i,
-    input 			       m_stb_i,
-    input [SEL_WIDTH-1:0] 	       m_sel_i,
-    input 			       m_we_i,
-    input [2:0] 		       m_cti_i,
-    input [1:0] 		       m_bte_i,
+    input [ADDR_WIDTH-1:0]             m_adr_i,
+    input [DATA_WIDTH-1:0]             m_dat_i,
+    input                              m_cyc_i,
+    input                              m_stb_i,
+    input [SEL_WIDTH-1:0]              m_sel_i,
+    input                              m_we_i,
+    input [2:0]                        m_cti_i,
+    input [1:0]                        m_bte_i,
 
     output reg [DATA_WIDTH-1:0]        m_dat_o,
-    output 			       m_ack_o,
-    output 			       m_err_o,
-    output 			       m_rty_o,
+    output                             m_ack_o,
+    output                             m_err_o,
+    output                             m_rty_o,
 
     output reg [ADDR_WIDTH*SLAVES-1:0] s_adr_o,
     output reg [DATA_WIDTH*SLAVES-1:0] s_dat_o,
-    output reg [SLAVES-1:0] 	       s_cyc_o,
-    output reg [SLAVES-1:0] 	       s_stb_o,
+    output reg [SLAVES-1:0]            s_cyc_o,
+    output reg [SLAVES-1:0]            s_stb_o,
     output reg [SEL_WIDTH*SLAVES-1:0]  s_sel_o,
-    output reg [SLAVES-1:0] 	       s_we_o,
-    output reg [SLAVES*3-1:0] 	       s_cti_o,
-    output reg [SLAVES*2-1:0] 	       s_bte_o,
+    output reg [SLAVES-1:0]            s_we_o,
+    output reg [SLAVES*3-1:0]          s_cti_o,
+    output reg [SLAVES*2-1:0]          s_bte_o,
 
     input [DATA_WIDTH*SLAVES-1:0]      s_dat_i,
-    input [SLAVES-1:0] 		       s_ack_i,
-    input [SLAVES-1:0] 		       s_err_i,
-    input [SLAVES-1:0] 		       s_rty_i
+    input [SLAVES-1:0]                 s_ack_i,
+    input [SLAVES-1:0]                 s_err_i,
+    input [SLAVES-1:0]                 s_rty_i
     );
    
    /* Derived local parameters */
@@ -150,32 +165,32 @@ module wb_decode
    // address and the memory range parameters
    generate
       if (SLAVES > 0)
-        assign s_select[0] = (m_adr_i[ADDR_WIDTH-1 -: S0_RANGE_WIDTH] == S0_RANGE_MATCH);
+        assign s_select[0] = S0_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S0_RANGE_WIDTH] == S0_RANGE_MATCH);
       if (SLAVES > 1)
-        assign s_select[1] = (m_adr_i[ADDR_WIDTH-1 -: S1_RANGE_WIDTH] == S1_RANGE_MATCH);
+        assign s_select[1] = S1_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S1_RANGE_WIDTH] == S1_RANGE_MATCH);
       if (SLAVES > 2)
-        assign s_select[2] = (m_adr_i[ADDR_WIDTH-1 -: S2_RANGE_WIDTH] == S2_RANGE_MATCH);
+        assign s_select[2] = S2_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S2_RANGE_WIDTH] == S2_RANGE_MATCH);
       if (SLAVES > 3)
-        assign s_select[3] = (m_adr_i[ADDR_WIDTH-1 -: S3_RANGE_WIDTH] == S3_RANGE_MATCH);
+        assign s_select[3] = S3_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S3_RANGE_WIDTH] == S3_RANGE_MATCH);
       if (SLAVES > 4)
-        assign s_select[4] = (m_adr_i[ADDR_WIDTH-1 -: S4_RANGE_WIDTH] == S4_RANGE_MATCH);
+        assign s_select[4] = S4_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S4_RANGE_WIDTH] == S4_RANGE_MATCH);
       if (SLAVES > 5)
-        assign s_select[5] = (m_adr_i[ADDR_WIDTH-1 -: S5_RANGE_WIDTH] == S5_RANGE_MATCH);
+        assign s_select[5] = S5_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S5_RANGE_WIDTH] == S5_RANGE_MATCH);
       if (SLAVES > 6)
-        assign s_select[6] = (m_adr_i[ADDR_WIDTH-1 -: S6_RANGE_WIDTH] == S6_RANGE_MATCH);
+        assign s_select[6] = S6_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S6_RANGE_WIDTH] == S6_RANGE_MATCH);
       if (SLAVES > 7)
-        assign s_select[7] = (m_adr_i[ADDR_WIDTH-1 -: S7_RANGE_WIDTH] == S7_RANGE_MATCH);
+        assign s_select[7] = S7_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S7_RANGE_WIDTH] == S7_RANGE_MATCH);
       if (SLAVES > 8)
-        assign s_select[8] = (m_adr_i[ADDR_WIDTH-1 -: S8_RANGE_WIDTH] == S8_RANGE_MATCH);
+        assign s_select[8] = S8_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S8_RANGE_WIDTH] == S8_RANGE_MATCH);
       if (SLAVES > 9)
-        assign s_select[9] = (m_adr_i[ADDR_WIDTH-1 -: S9_RANGE_WIDTH] == S9_RANGE_MATCH);
+        assign s_select[9] = S9_ENABLE & (m_adr_i[ADDR_WIDTH-1 -: S9_RANGE_WIDTH] == S9_RANGE_MATCH);
    endgenerate
    
    // If two s_select are high or none, we might have an bus error
    wire                            bus_error;
    assign bus_error = ~^s_select;
 
-   reg 				   m_ack, m_err, m_rty;
+   reg                             m_ack, m_err, m_rty;
    
    // Mux the slave bus based on the slave select signal (one hot!)
    always @(*) begin : bus_s_mux
