010cd16cc1115614f386d8ae910b6d6d0dcafbe7
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-08-21 17:21:57 +0200

    WIP: DEM-UART extended functionality
    * Add interrupts
    
    * Prepare further features

--- external/opensocdebug/hardware/modules/dem_uart/common/osd_dem_uart_16550.sv
+++ external/opensocdebug/hardware/modules/dem_uart/common/osd_dem_uart_16550.sv
@@ -19,79 +19,128 @@ import dii_package::dii_flit;
 module osd_dem_uart_16550
   (input clk, rst,
 
-   (* mark_debug = "yes" *) input            bus_req,
-   (* mark_debug = "yes" *) input [2:0]      bus_addr,
-   (* mark_debug = "yes" *) input            bus_write,
-   (* mark_debug = "yes" *) input [7:0]      bus_wdata,
-   (* mark_debug = "yes" *) output           bus_ack,
-   (* mark_debug = "yes" *) output reg [7:0] bus_rdata,
+   input            bus_req,
+   input [2:0]      bus_addr,
+   input            bus_write,
+   input [7:0]      bus_wdata,
+   output           bus_ack,
+   output reg [7:0] bus_rdata,
 
-   (* mark_debug = "yes" *) input            drop,
+   input            drop,
 
-   (* mark_debug = "yes" *) output           out_valid,
-   (* mark_debug = "yes" *) output [7:0]     out_char,
-   (* mark_debug = "yes" *) input            out_ready,
-   (* mark_debug = "yes" *) input            in_valid,
-   (* mark_debug = "yes" *) input [7:0]      in_char,
-   (* mark_debug = "yes" *) output           in_ready,
-   
-   (* mark_debug = "yes" *) output           irq);
+   output reg       out_valid,
+   output reg [7:0] out_char,
+   input            out_ready,
+   input            in_valid,
+   input [7:0]      in_char,
+   output reg       in_ready,
+
+   output           irq);
 
    localparam REG_TXRX = 0;
    localparam REG_IER = 1;
-   localparam REG_IIR = 2;
+   localparam REG_IIR_FCR = 2;
    localparam REG_LCR = 3;
    localparam REG_LSR = 5;
 
-   // DLAB: 
+   reg bus_resp;
+
+   assign bus_ack = bus_resp;
+
+   // DLAB:
    // 0: RBR, THR and IER accessible (during communicatino)
    // 1: DLL and DLM accessible (during initialization to set baud rate)
-   (* mark_debug = "yes" *) reg          lcr_7; 
+   reg          dlab;
 
-   // trigger an interrupt if we are ready for TX data from the software (THE)
-   (* mark_debug = "yes" *) reg enable_tx_empty_interrupt;
-   
-   // we're always ready to get data whenever the CPU is ready ...
-   // XXX: make this depending on the NoC status
-   assign irq = enable_tx_empty_interrupt;
-   
-   always @(posedge clk)
-     if (rst) begin
-       lcr_7 <= 0;
-       enable_tx_empty_interrupt <= 0;
-     end else if (bus_req & bus_write & (bus_addr == REG_LCR))
-       lcr_7 <= bus_wdata[7];
-     else if (bus_req & bus_write & (bus_addr == REG_IER) & lcr_7 == 0)
-        enable_tx_empty_interrupt <= bus_wdata[1];
+   // Interrupt enable registers
+   reg          erbfi; // Receive Data Available Interrupt
+   reg          etbei; // Transmitter Holding Register Empty Interrupt
+   reg          elsi;  // Receiver Line Status Interrupt
 
-   assign out_valid = bus_req & bus_write & (bus_addr == REG_TXRX) & !lcr_7;
-   assign out_char = bus_wdata;
+   // Interrupts
+   logic        irq_ls, irq_rbf, irq_tbe;
 
-   always @(*)
-     if (!bus_write) begin
-         case (bus_addr)
-           REG_TXRX: bus_rdata = in_char;
-           REG_LSR: bus_rdata = {7'h30, in_valid}; // 0x30 == THR is empty AND THR is empty, and line is idle 
-           REG_IIR: begin
-              if (lcr_7 /* DLAB */ == 0) begin
-                 // we trigger only one interrupt, hardcode the interrupt reason to "transmit empty"
-                 bus_rdata = 8'h0;
-                 bus_rdata[3:1] = 3'b001; // Transmit Holding Register Interrupt (THRE) 
-              end
-           end
-           REG_IER: begin
-              if (lcr_7 /* DLAB */ == 0) begin
-                 bus_rdata = 8'h0;
-                 bus_rdata[1] = enable_tx_empty_interrupt; // enable readback, it's required by the Linux kernel
-              end
-           end
-           default: bus_rdata = 8'h0;
-         endcase
-     end
+   assign irq = irq_ls | irq_rbf | irq_tbe;
 
-   assign bus_ack = lcr_7 | (bus_addr != REG_TXRX) |
-                    (bus_write ? (out_ready | drop) : 1'b1);
+   assign irq_tbe = etbei & out_ready;
+   assign irq_rbf = erbfi & in_valid;
+   assign irq_ls = 0;
 
-   assign in_ready = bus_req & !bus_write & (bus_addr == REG_TXRX);
+   // FIFO
+   reg          fifo_enable, fifo_rcvr_enable, fifo_xmit_enable, dma_mode;
+
+   always @(posedge clk) begin
+      if (rst) begin
+         dlab <= 0;
+         bus_resp <= 0;
+         out_valid <= 0;
+         in_ready <= 0;
+         {erbfi, etbei, elsi} <= 0;
+         {fifo_enable, fifo_rcvr_enable, fifo_xmit_enable, dma_mode} <= 0;
+      end else begin
+         out_valid <= 0;
+         in_ready <= 0;
+
+         if (bus_resp) begin
+            bus_resp <= 0;
+         end else if (bus_req) begin
+            bus_resp <= 1;
+
+            if (dlab) begin
+               case (bus_addr)
+                 REG_LCR: begin
+                    if (bus_write) begin
+                       dlab <= bus_wdata[7];
+                    end
+                 end
+               endcase // case (bus_addr)
+            end else begin
+               case (bus_addr)
+                 REG_TXRX: begin
+                    bus_rdata <= in_char;
+
+                    if (bus_write) begin
+                       out_valid <= 1;
+                       out_char <= bus_wdata;
+                    end else begin
+                       in_ready <= 1;
+                    end
+                 end
+                 REG_LCR: begin
+                    if (bus_write) begin
+                       dlab <= bus_wdata[7];
+                    end
+                 end
+                 REG_IER: begin
+                    bus_rdata <= {5'h0, elsi, etbei, erbfi};
+                    if (bus_write) begin
+                       {elsi, etbei, erbfi} <= bus_wdata[2:0];
+                    end
+                 end
+                 REG_IIR_FCR: begin
+                    bus_rdata[7:3] <= 5'h0;
+                    if (irq_ls)
+                      bus_rdata[2:0] <= 3'b110;
+                    else if (irq_rbf)
+                      bus_rdata[2:0] <= 3'b100;
+                    else if (irq_tbe)
+                      bus_rdata[2:0] <= 3'b010;
+                    else
+                      bus_rdata[2:0] <= 3'b001;
+
+                    if (bus_write) begin
+                       // FCR
+                       {fifo_enable, fifo_rcvr_enable, fifo_xmit_enable, dma_mode} <= bus_wdata[3:0];
+                    end
+                 end // case: REG_IIR_FCR
+               REG_LSR: begin
+                   // TMTE=1, THRE=1, DR (data ready) when input
+                  bus_rdata <= {7'h30, in_valid};
+               end
+               endcase // case (bus_addr)
+            end
+         end // if (bus_req)
+      end
+   end
 
 endmodule // osd_dem_uart_16550
