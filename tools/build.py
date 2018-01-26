600c7dc38e967ebe36d565abb74abce6fd84de57
Philipp Wagner <mail@philipp-wagner.com>
2018-01-26 10:49:25 +0100

    Adjust build.py for new OSD Software

--- tools/build.py
+++ tools/build.py
@@ -772,7 +772,7 @@ def build_externals_opensocdebug_software(options, env):
     info(" + Configure")
     ensure_directory(objdir)
 
-    cmd = "{}/configure --prefix={} --enable-python-bindings".format(src, prefix)
+    cmd = "{}/configure --prefix={} --enable-logging --with-glip".format(src, prefix)
     run_command(cmd, cwd=objdir, env=env)
 
     info(" + Build")
