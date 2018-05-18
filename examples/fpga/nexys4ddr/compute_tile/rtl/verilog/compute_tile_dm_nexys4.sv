437471b45163f971be319bc063f89250c5e57e4c
Philipp Wagner <philipp.wagner@tum.de>
2018-05-18 14:47:19 +0200

    compute_tile_dm_nexys4ddr: Explicitly set parameters
    This increases readability.

--- examples/fpga/nexys4ddr/compute_tile/rtl/verilog/compute_tile_dm_nexys4.sv
+++ examples/fpga/nexys4ddr/compute_tile/rtl/verilog/compute_tile_dm_nexys4.sv
@@ -212,7 +212,9 @@ module compute_tile_dm_nexys4
    // Single compute tile with all memory mapped to the DRAM
    compute_tile_dm
       #(.CONFIG(CONFIG),
-        .DEBUG_BASEID((CONFIG.DEBUG_LOCAL_SUBNET << (16 - CONFIG.DEBUG_SUBNET_BITS)) + 1)
+        .DEBUG_BASEID((CONFIG.DEBUG_LOCAL_SUBNET << (16 - CONFIG.DEBUG_SUBNET_BITS)) + 1),
+        .ID(0),
+        .COREBASE(0)
       )
       u_compute_tile
         (
